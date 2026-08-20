684e80b3f22f6b35362fa1562fe5bf6b8786ae67
Robert Locsin <59687824+roburp@users.noreply.github.com>
2026-08-20 16:10:46 +0800

    Add capitalize function and tests

---
+++ src/capitalize.js
@@ -0,0 +1,4 @@
+// A capitalize function that takes a string and returns it with the first character capitalized.
+export function capitalize(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
---
+++ tests/capitalize.test.js
@@ -0,0 +1,26 @@
+//import { capitalize } from "../src/capitalize.js";
+import { capitalize } from "../src/capitalize.js";
+
+test("empty string", () => {
+  expect(capitalize("")).toBe("");
+});
+
+test("one character", () => {
+  expect(capitalize("a")).toBe("A");
+});
+
+test("leading space", () => {
+  expect(capitalize(" a")).toBe(" a");
+});
+
+test("one word", () => {
+  expect(capitalize("hello")).toBe("Hello");
+});
+
+test("multiple words", () => {
+  expect(capitalize("hello world")).toBe("Hello world");
+});
+
+test("numbers", () => {
+  expect(capitalize("123")).toBe("123");
+});
